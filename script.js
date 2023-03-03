a4dc48fb291441c109fda663cf827d44f3e24298
Aryan <aryanbiswari14@gmail.com>
2023-03-03 12:49:52 +0530

    added duration

--- script.js
+++ script.js
@@ -46,10 +46,42 @@ xhr.onreadystatechange = function () {
       });
     }
 
+    // Set the song cover to the image of the first track on the playlist
+    document.getElementById("song-cover").src =
+      playlist[0].getElementsByTagName("images")[0].childNodes[0].nodeValue;
+
+    // Set the song title and artist to the details of the first track on the playlist
+    document.getElementById("song-title").textContent =
+      playlist[0].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    document.getElementById("song-artist").textContent =
+      playlist[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+
+    // Set the source of the audio player to the first track on the playlist
+    audioPlayer.src =
+      playlist[0].getElementsByTagName("file")[0].childNodes[0].nodeValue;
+
+    //set duration
+    function updateProgress() {
+      const currentTime = document.getElementById("song-current-time");
+      const totalTime = document.getElementById("song-total-time");
+      const duration = audioPlayer.duration;
+      const currentTimeFormatted = formatTime(audioPlayer.currentTime);
+      const totalTimeFormatted = formatTime(duration);
+      currentTime.textContent = currentTimeFormatted;
+      totalTime.textContent = totalTimeFormatted;
+    }
+
+    function formatTime(time) {
+      const minutes = Math.floor(time / 60);
+      const seconds = Math.floor(time % 60);
+      return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    }
+
     function playMusic() {
       audioPlayer.play();
       document.getElementById("play-pause").innerHTML =
         "<i class='fas fa-pause'></i>";
+      setInterval(updateProgress, 1000);
     }
 
     function pauseMusic() {
@@ -58,7 +90,9 @@ xhr.onreadystatechange = function () {
         "<i class='fas fa-play'></i>";
     }
 
-    document.getElementById("play-pause").addEventListener("click", function () {
+    document
+      .getElementById("play-pause")
+      .addEventListener("click", function () {
         if (audioPlayer.paused) {
           playMusic();
           document.getElementById("play-pause").innerHTML =
@@ -69,45 +103,44 @@ xhr.onreadystatechange = function () {
             "<i class='fas fa-play'></i>";
         }
       });
-      
 
+    function playTrack(trackIndex) {
+      const playlist = xmlDoc.getElementsByTagName("track");
+      const track = playlist[trackIndex];
+      const audioPlayer = new Audio();
+      document.getElementById("song-cover").src =
+        track.getElementsByTagName("images")[0].childNodes[0].nodeValue;
+      document.getElementById("song-title").textContent =
+        track.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+      document.getElementById("song-artist").textContent =
+        track.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+      audioPlayer.src =
+        track.getElementsByTagName("file")[0].childNodes[0].nodeValue;
+      audioPlayer.play();
+      return audioPlayer;
+    }
 
-
-      function playTrack(trackIndex) {
-        const playlist = xmlDoc.getElementsByTagName("track");
-        const track = playlist[trackIndex];
-        const audioPlayer = new Audio();
-        document.getElementById("song-cover").src = track.getElementsByTagName("images")[0].childNodes[0].nodeValue;
-        document.getElementById("song-title").textContent = track.getElementsByTagName("name")[0].childNodes[0].nodeValue;
-        document.getElementById("song-artist").textContent = track.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
-        audioPlayer.src = track.getElementsByTagName("file")[0].childNodes[0].nodeValue;
-        audioPlayer.play();
-        return audioPlayer;
+    document.getElementById("next").addEventListener("click", function () {
+      currentTrackIndex++;
+      if (currentTrackIndex >= playlistCount) {
+        currentTrackIndex = 0;
       }
+      audioPlayer.pause();
+      audioPlayer = playTrack(currentTrackIndex);
+      document.getElementById("play-pause").innerHTML =
+        "<i class='fas fa-pause'></i>";
+    });
 
-
-
-
-      document.getElementById("next").addEventListener("click", function () {
-        currentTrackIndex++;
-        if (currentTrackIndex >= playlistCount) {
-          currentTrackIndex = 0;
-        }
-        audioPlayer.pause();
-        audioPlayer = playTrack(currentTrackIndex);
-        document.getElementById("play-pause").innerHTML = "<i class='fas fa-pause'></i>";
-      });
-      
-      document.getElementById("previous").addEventListener("click", function () {
-        currentTrackIndex--;
-        if (currentTrackIndex < 0) {
-          currentTrackIndex = playlistCount - 1;
-        }
-        audioPlayer.pause();
-        audioPlayer = playTrack(currentTrackIndex);
-        document.getElementById("play-pause").innerHTML = "<i class='fas fa-pause'></i>";
-      });
-      
+    document.getElementById("previous").addEventListener("click", function () {
+      currentTrackIndex--;
+      if (currentTrackIndex < 0) {
+        currentTrackIndex = playlistCount - 1;
+      }
+      audioPlayer.pause();
+      audioPlayer = playTrack(currentTrackIndex);
+      document.getElementById("play-pause").innerHTML =
+        "<i class='fas fa-pause'></i>";
+    });
   }
 };
 
